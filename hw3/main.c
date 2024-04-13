39d375086bd21db0acb7125d86b147c0f3d4b102
Tzu-Ching Yang <shingekinocore@gmail.com>
2024-04-13 09:29:34 +0000

    Implement sudoku algorithm in C

--- hw3/main.c
+++ hw3/main.c
@@ -1,61 +1,77 @@
-//main.c
+// main.c
+#include "sudoku_2x2_c.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "sudoku_2x2_c.h"
 #define SIZE 16
 
-char test_c_data[16] = { 0, 0, 2, 0, 
-                         0, 0, 0, 4,
-                         2, 3, 0, 0, 
-                         0, 4, 0, 0 };
-                      
-char test_asm_data[16] = { 0, 0, 2, 0, 
-                           0, 0, 0, 4,
-                           2, 3, 0, 0, 
-                           0, 4, 0, 0 };
+char test_c_data[16] = {0, 0, 2, 0,
+                        0, 0, 0, 4,
+                        2, 3, 0, 0,
+                        0, 4, 0, 0};
 
-void print_sudoku_result() {
-    int i;
-    char str[25];
-    puts("Output c & assembly function result\n");
-    puts("c result :\n");
-    for( i=0 ; i<SIZE ; i++) {   
-        int j= *(test_c_data+i);
-        itoa(j, str, 10);
-        puts(str);
-    }
+char test_asm_data[16] = {0, 0, 2, 0, 0, 0, 0, 4, 2, 3, 0, 0, 0, 4, 0, 0};
 
-    puts("\n\nassembly result :\n");
-    for( i=0 ; i<SIZE ; i++) {
-        int j= *(test_asm_data+i);
-        itoa(j, str, 10);
-        puts(str);
-    }
-
-    int flag = 0;
-    for( i=0 ; i<SIZE ; i++) {
-        if (*(test_c_data+i) != *(test_asm_data+i)) {
-            flag = 1;
-            break;
-        }
-    }
-
-    if (flag == 1){
-        puts("\n\nyour c & assembly got different result ... QQ ...\n");
-    }
-    else {
-        puts("\n\nyour c & assembly got same result!\n");
-    }
+void print_meow() {
+  puts("Meow\n");
+  // fflush(stdout);
 }
 
+void print_val(int v) {
+  char str[25];
+  itoa(v, str, 10);
+  puts("val: ");
+  puts(str);
+  puts("\n");
+}
 
-void sudoku_2x2_asm(char *test_asm_data); // TODO, sudoku_2x2_asm.S
+void print_single_result(char *ls) {
+  char str[25];
 
-void sudoku_2x2_c(char *test_c_data); // TODO, sudoku_2x2_c.S
-                        
+  for (int i = 0; i < 4; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      int n = *(ls + i * 4 + j);
+      itoa(n, str, 10);
+      puts(str);
+      puts(" ");
+    }
+    puts("\n");
+  }
+  puts("\n");
+}
+
+void print_sudoku_result() {
+  int i;
+  puts("Output c & assembly function result\n");
+  puts("c result :\n");
+  print_single_result(test_c_data);
+
+  puts("assembly result :\n");
+  print_single_result(test_asm_data);
+
+  int flag = 0;
+  for (i = 0; i < SIZE; i++) {
+    if (*(test_c_data + i) != *(test_asm_data + i)) {
+      flag = 1;
+      break;
+    }
+  }
+
+  if (flag == 1) {
+    puts("\nyour c & assembly got different result ... QQ ...\n");
+  } else {
+    puts("\n\nyour c & assembly got same result!\n");
+  }
+}
+
+int sudoku_2x2_asm(char *test_asm_data); // TODO, sudoku_2x2_asm.S
+
+int sudoku_2x2_c(char *test_c_data); // TODO, sudoku_2x2_c.S
+
 int main() {
-    sudoku_2x2_c(test_c_data);
-    sudoku_2x2_asm(test_asm_data);
-    print_sudoku_result();
-    return 0;
+  // print_val(
+  // );
+  sudoku_2x2_c(test_c_data);
+  print_val(sudoku_2x2_asm(test_asm_data));
+  print_sudoku_result();
+  return 0;
 }
--- hw3/sudoku_2x2_c.c
+++ hw3/sudoku_2x2_c.c
@@ -1,9 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void sudoku_2x2_c(char *test_c_data){
+const int box_width = 2;
+const int side_lim = 4;
+const int bound = 16;
 
-// TODO
-// Finish your sudoku algorithm in c language
+#define true 1
+#define false 0
 
+static inline int check_and_mark(char shift, int *tb) {
+  int hot = 1 << shift;
+  if (shift && ((*tb) & hot))
+    return false;
+  *tb = (*tb) | hot;
+  return true;
 }
+
+static inline int check(char *ls, int idx) {
+  int x = idx % side_lim;
+  int y = idx / side_lim;
+
+  // horizontal scan by fixing y (in single dimension version)
+  int base = y * side_lim;
+  int tb = 0;
+  for (int i = base; i < base + side_lim; ++i) {
+    if (!check_and_mark(ls[i], &tb)) {
+      return false;
+    }
+  }
+
+  // vertical scan by fixing x
+  base = x;
+  tb = 0;
+  for (int i = base; i < bound; i += side_lim) {
+    if (!check_and_mark(ls[i], &tb)) {
+      return false;
+    }
+  }
+
+  // box scan by indirect index mapping
+  tb = 0;
+  int baseX = (x / box_width) * box_width;
+  int baseY = (y / box_width) * box_width;
+  for (int i = 0; i < box_width; ++i) {
+    int offset = (i + baseY) * side_lim + baseX;
+    for (int j = 0; j < box_width; ++j) {
+      if (!check_and_mark(ls[offset + j], &tb)) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+static inline int solve(char *ls, int idx) {
+  if (idx == bound) {
+    return true;
+  }
+
+  if (ls[idx] > 0) {
+    return solve(ls, idx + 1);
+  } else {
+    for (char n = 1; n <= 4; ++n) {
+      ls[idx] = n; // mark
+      if (check(ls, idx) && solve(ls, idx + 1)) {
+        return true;
+      }
+    }
+    ls[idx] = 0;
+    return false;
+  }
+}
+
+int sudoku_2x2_c(char *ls) { return solve(ls, 0); }
+
+// int main() {
+//   char test_c_data[16] = {0, 2, 0, 3, 1, 3, 4, 0, 2, 0, 0, 4, 3, 0, 2, 0};
+
+//   int is_success = sudoku_2x2_c(test_c_data);
+//   printf("Success: %d\n", is_success);
+//   for (int i = 0; i < side_lim; ++i) {
+//       for (int j = 0; j < side_lim; ++j) {
+//           printf("%c ", test_c_data[i * side_lim + j] + '0');
+//       }
+//       printf("\n");
+//   }
+// }
